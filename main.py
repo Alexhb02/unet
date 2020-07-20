feaa5ec390de20d68f9aadd0bec52a697ac4b451
Alexhb02 <Alexanderhb02@gmail.com>
2020-07-20 14:00:07 -0700

    Added evaluation segmentation

--- main.py
+++ main.py
@@ -1,22 +1,152 @@
 from model import *
 from data import *
+from PIL import Image
+import numpy as np
+import imageio
+import os, os.path
+from matplotlib import image
+from matplotlib import pyplot
 
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+                     width_shift_range=0.05,
+                     height_shift_range=0.05,
+                     shear_range=0.05,
+                     zoom_range=0.05,
+                     horizontal_flip=True,
+                     fill_mode='nearest')
+myGene = trainGenerator(2, 'data/membrane/train', 'image', 'label', data_gen_args, save_to_dir=None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss', verbose=1, save_best_only=True)
+model.fit_generator(myGene, steps_per_epoch=300, epochs=1, callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+results = model.predict_generator(testGene, 30, verbose=1)
+saveResult("data/membrane/test", results)
+
+
+def pixel_accuracy(eval_segm, gt_segm):
+    # sum_i(n_ii) / sum_i(t_i)
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    sum_n_ii = 0
+    sum_t_i = 0
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        sum_n_ii += np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        sum_t_i += np.sum(curr_gt_mask)
+
+    if (sum_t_i == 0):
+        pixel_accuracy_ = 0
+    else:
+        pixel_accuracy_ = sum_n_ii / sum_t_i
+
+    return pixel_accuracy_
+
+
+def get_pixel_area(segm):
+    return segm.shape[0] * segm.shape[1]
+
+
+def extract_both_masks(eval_segm, gt_segm, cl, n_cl):
+    eval_mask = extract_masks(eval_segm, cl, n_cl)
+    gt_mask = extract_masks(gt_segm, cl, n_cl)
+
+    return eval_mask, gt_mask
+
+
+def extract_classes(segm):
+    cl = np.unique(segm)
+    n_cl = len(cl)
+
+    return cl, n_cl
+
+
+def union_classes(eval_segm, gt_segm):
+    eval_cl, _ = extract_classes(eval_segm)
+    gt_cl, _ = extract_classes(gt_segm)
+
+    cl = np.union1d(eval_cl, gt_cl)
+    n_cl = len(cl)
+
+    return cl, n_cl
+
+
+def extract_masks(segm, cl, n_cl):
+    h, w = segm_size(segm)
+    masks = np.zeros((n_cl, h, w))
+
+    for i, c in enumerate(cl):
+        masks[i, :, :] = segm == c
+
+    return masks
+
+
+def segm_size(segm):
+    try:
+        height = segm.shape[0]
+        width = segm.shape[1]
+    except IndexError:
+        raise
+
+    return height, width
+
+
+def check_size(eval_segm, gt_segm):
+    h_e, w_e = segm_size(eval_segm)
+    h_g, w_g = segm_size(gt_segm)
+
+    if (h_e != h_g) or (w_e != w_g):
+        raise EvalSegErr("DiffDim: Different dimensions of matrices!")
+
+
+def mean_IU(eval_segm, gt_segm):
+    '''
+    (1/n_cl) * sum_i(n_ii / (t_i + sum_j(n_ji) - n_ii))
+    '''
+
+    check_size(eval_segm, gt_segm)
+
+    cl, n_cl = union_classes(eval_segm, gt_segm)
+    _, n_cl_gt = extract_classes(gt_segm)
+    eval_mask, gt_mask = extract_both_masks(eval_segm, gt_segm, cl, n_cl)
+
+    IU = list([0]) * n_cl
+
+    for i, c in enumerate(cl):
+        curr_eval_mask = eval_mask[i, :, :]
+        curr_gt_mask = gt_mask[i, :, :]
+
+        if (np.sum(curr_eval_mask) == 0) or (np.sum(curr_gt_mask) == 0):
+            continue
+
+        n_ii = np.sum(np.logical_and(curr_eval_mask, curr_gt_mask))
+        t_i = np.sum(curr_gt_mask)
+        n_ij = np.sum(curr_eval_mask)
+
+        IU[i] = n_ii / (t_i + n_ij - n_ii)
+
+    mean_IU_ = np.sum(IU) / n_cl_gt
+    return mean_IU_
+
+
+sumPixAcc = 0
+sumMeanIU = 0
+for num in range(0, len(os.listdir("data/membrane/test_label")) - 1):
+    image_pred = imageio.imread("data/membrane/test/" + str(num) + "_predict.png")
+    image = imageio.imread("data/membrane/test_label/" + str(num) + ".png")
+    sumPixAcc += pixel_accuracy(image, image_pred)
+    sumMeanIU += mean_IU(image, image_pred)
+
+print("Average Pixel Accuracy: ", sumPixAcc / len(os.listdir("data/membrane/test_label")))
+print("Average Mean IU: ", sumMeanIU / len(os.listdir("data/membrane/test_label")))
